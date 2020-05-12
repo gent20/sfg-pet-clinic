e45a611cf3fb7faf5c6a97b8c7505a2614271327
Gent <gentkastratio@gmail.com>
2020-05-12 22:52:12 +0200

    Initial Project add

---
+++ src/main/java/gent/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package gent.springframework.sfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicApplication.class, args);
+    }
+
+}
