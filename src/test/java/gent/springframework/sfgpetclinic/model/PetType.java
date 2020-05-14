37f038611a3cbc46f50b1ca8eafecef7cc4ab62f
Gent <gentkastratio@gmail.com>
2020-05-14 16:52:30 +0200

    added POJOs 2# closed

---
+++ src/test/java/gent/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,14 @@
+package gent.springframework.sfgpetclinic.model;
+
+public class PetType {
+
+    private String petName;
+
+    public String getPetName() {
+        return petName;
+    }
+
+    public void setPetName(String petName) {
+        this.petName = petName;
+    }
+}
