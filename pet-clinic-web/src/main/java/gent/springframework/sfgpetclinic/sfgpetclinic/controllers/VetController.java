8e87ec72d13e7aa848854900619465e044cf3c01
gent20 <123qweasdbnM>
2020-06-25 19:20:43 +0200

    created an vet index html file andcontroller. closes #7

---
+++ pet-clinic-web/src/main/java/gent/springframework/sfgpetclinic/sfgpetclinic/controllers/VetController.java
@@ -0,0 +1,19 @@
+/*
+ * Created by Gent Kastrati
+ */
+
+package gent.springframework.sfgpetclinic.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+
+    @RequestMapping({"/vets","/vets/index","/vets/index.html"})
+    public String listVets(){
+        return "vets/index";
+    }
+
+}
