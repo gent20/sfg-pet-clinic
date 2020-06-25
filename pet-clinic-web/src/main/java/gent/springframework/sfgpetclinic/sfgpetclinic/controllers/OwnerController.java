a2cd4514f0face21614c052fbb18ca0be2220a64
gent20 <123qweasdbnM>
2020-06-25 19:27:55 +0200

    created an owner index html file andcontroller. closes #13

---
+++ pet-clinic-web/src/main/java/gent/springframework/sfgpetclinic/sfgpetclinic/controllers/OwnerController.java
@@ -0,0 +1,19 @@
+/*
+ * Created by Gent Kastrati
+ */
+
+package gent.springframework.sfgpetclinic.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"","/","/index","/index.html"})
+    public String listOwners(){
+
+        return "index/owners";
+    }
+}
