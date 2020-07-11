77a3dfcb4b7b8ca81bf8b130c543173ad2341eb3
gent20 <123qweasdbnM>
2020-07-11 17:26:29 +0200

    Closs #29

---
+++ pet-clinic-data/src/main/java/gent/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,15 @@
+/*
+ * Created by Gent Kastrati
+ */
+
+/*
+ * Created by Gent Kastrati
+ */
+
+package gent.springframework.sfgpetclinic.services;
+
+import gent.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/gent/springframework/sfgpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,41 @@
+/*
+ * Created by Gent Kastrati
+ */
+
+package gent.springframework.sfgpetclinic.services.map;
+
+import gent.springframework.sfgpetclinic.model.PetType;
+import gent.springframework.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType ,Long> implements PetTypeService {
+
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteByID(id);
+    }
+}
