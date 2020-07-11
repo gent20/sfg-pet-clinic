42e06f8d1d6c2b4039f3d238e330ce79711047d1
gent20 <123qweasdbnM>
2020-07-11 15:58:18 +0200

    Closs #26

--- pet-clinic-data/src/main/java/gent/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/gent/springframework/sfgpetclinic/model/Owner.java
@@ -1,12 +1,20 @@
-
 /*
  * Created by Gent Kastrati
  */
 
 package gent.springframework.sfgpetclinic.model;
-
+import java.util.Set;
 
 public class Owner extends Person{
 
+    private Set<Pet> pets;
 
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
