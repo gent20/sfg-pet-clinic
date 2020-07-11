00a76b74c7e4c3360670ac02e922ea473eafc449
gent20 <123qweasdbnM>
2020-07-11 16:02:24 +0200

    Closs #27

---
+++ pet-clinic-data/src/main/java/gent/springframework/sfgpetclinic/model/Specialty.java
@@ -0,0 +1,18 @@
+/*
+ * Created by Gent Kastrati
+ */
+
+package gent.springframework.sfgpetclinic.model;
+
+public class Specialty extends BaseEntity{
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
