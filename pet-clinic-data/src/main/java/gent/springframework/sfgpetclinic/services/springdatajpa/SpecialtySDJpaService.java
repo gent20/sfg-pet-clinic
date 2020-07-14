54697489122122c7e031f00e83fbeef88fd19104
gent20 <123qweasdbnM>
2020-07-14 23:37:12 +0200

    create spring data jpa service for Specialty .closes #43

---
+++ pet-clinic-data/src/main/java/gent/springframework/sfgpetclinic/services/springdatajpa/SpecialtySDJpaService.java
@@ -0,0 +1,54 @@
+/*
+ * Created by Gent Kastrati
+ */
+
+package gent.springframework.sfgpetclinic.services.springdatajpa;
+
+
+import gent.springframework.repositories.SpecialtyRepository;
+import gent.springframework.sfgpetclinic.model.Specialty;
+import gent.springframework.sfgpetclinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialtySDJpaService implements SpecialtyService {
+
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialtySDJpaService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        Set<Specialty> specialties = new HashSet<>();
+
+        specialtyRepository.findAll().forEach(specialties::add);
+        return specialties;
+    }
+
+    @Override
+    public Specialty findById(Long aLong) {
+        return specialtyRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialtyRepository.deleteById(aLong);
+    }
+}
