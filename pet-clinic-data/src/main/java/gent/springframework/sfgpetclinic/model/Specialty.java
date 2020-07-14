4d458c1f3238afe17a91ed1bcc7636c224424421
gent20 <123qweasdbnM>
2020-07-14 15:00:42 +0200

    Converted Vet into JPA Entity. Closes #37

--- pet-clinic-data/src/main/java/gent/springframework/sfgpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/gent/springframework/sfgpetclinic/model/Specialty.java
@@ -4,9 +4,18 @@
 
 package gent.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity{
 
+    @Column(name = "description")
     private String description;
+
 
     public String getDescription() {
         return description;
--- pet-clinic-data/src/main/java/gent/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/gent/springframework/sfgpetclinic/model/Vet.java
@@ -1,11 +1,18 @@
 package gent.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties",joinColumns = @JoinColumn(name = "vet_id"),
+                inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialties = new HashSet<>();
+
 
     public Set<Specialty> getSpecialties() {
         return specialties;
