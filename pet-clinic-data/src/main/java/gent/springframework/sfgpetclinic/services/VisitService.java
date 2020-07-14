9995e43f27b6f17389de29528b1d05eadd9fa9e2
gent20 <123qweasdbnM>
2020-07-14 23:52:47 +0200

    create visit map service .closes #44

---
+++ pet-clinic-data/src/main/java/gent/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,10 @@
+/*
+ * Created by Gent Kastrati
+ */
+
+package gent.springframework.sfgpetclinic.services;
+
+import gent.springframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit,Long>{
+}
