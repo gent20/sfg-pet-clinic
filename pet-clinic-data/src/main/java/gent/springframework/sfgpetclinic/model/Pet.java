6937969dbaf3c21a2290692dbdc1aeb707165977
gent20 <123qweasdbnM>
2020-07-14 14:48:19 +0200

    Converted owner into JPA Entity. Closes #36

--- pet-clinic-data/src/main/java/gent/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/gent/springframework/sfgpetclinic/model/Pet.java
@@ -1,13 +1,26 @@
 package gent.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birthday")
     private LocalDate birthday;
+
 
 
     public String getName() {
--- pet-clinic-data/src/main/java/gent/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/gent/springframework/sfgpetclinic/model/PetType.java
@@ -1,7 +1,15 @@
 package gent.springframework.sfgpetclinic.model;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity{
 
+    @Column(name = "pet_name")
     private String petName;
 
     public String getPetName() {
