201cf749e47f6bd15d7b36fbe9af6f3718ed806e
gent20 <123qweasdbnM>
2020-07-19 16:15:10 +0200

    created a test for the owner controller .closes #50

--- pet-clinic-data/src/main/test/gent/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
+++ pet-clinic-data/src/main/test/gent/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
@@ -23,12 +23,12 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.client.ExpectedCount.times;
 
 @ExtendWith(MockitoExtension.class)
 class OwnerSDJpaServiceTest {
 
     public static final String LAST_NAME = "Smith";
+
     @Mock
     OwnerRepository ownerRepository;
 
