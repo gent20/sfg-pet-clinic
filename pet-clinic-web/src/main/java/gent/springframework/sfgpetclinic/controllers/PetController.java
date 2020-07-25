e2873ba7f8a50875320b2c8328e403f4b4efa720
gent20 <123qweasdbnM>
2020-07-25 22:18:28 +0200

    added model attributes . #61

---
+++ pet-clinic-web/src/main/java/gent/springframework/sfgpetclinic/controllers/PetController.java
@@ -0,0 +1,49 @@
+package gent.springframework.sfgpetclinic.controllers;
+
+import gent.springframework.sfgpetclinic.model.Owner;
+import gent.springframework.sfgpetclinic.model.Pet;
+import gent.springframework.sfgpetclinic.model.PetType;
+import gent.springframework.sfgpetclinic.services.OwnerService;
+import gent.springframework.sfgpetclinic.services.PetService;
+import gent.springframework.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+
+
+@Controller
+@RequestMapping("/owners/{ownerId}")
+public class PetController {
+
+    private static final String VIEWS_PETS_CREATE_OR_UPDATE_FORM = "pets/createOrUpdatePetForm";
+
+    private final PetService petService;
+    private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
+
+    public PetController(PetService petService, OwnerService ownerService, PetTypeService petTypeService) {
+        this.petService = petService;
+        this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
+    }
+
+    @ModelAttribute("types")
+    public Collection<PetType> populatePetTypes() {
+        return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable("ownerId") Long ownerId) {
+        return ownerService.findById(ownerId);
+    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+}
