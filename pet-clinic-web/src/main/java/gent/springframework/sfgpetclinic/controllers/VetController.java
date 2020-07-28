75303be3ba9e3c65c09ef7941980edddcf948db4
gent20 <123qweasdbnM>
2020-07-28 14:51:20 +0200

    implemented vets json endpoint . #68

--- pet-clinic-web/src/main/java/gent/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/gent/springframework/sfgpetclinic/controllers/VetController.java
@@ -8,10 +8,15 @@
 
 package gent.springframework.sfgpetclinic.controllers;
 
+import gent.springframework.sfgpetclinic.model.Vet;
 import gent.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -27,6 +32,11 @@ public class VetController {
 
         model.addAttribute("vets",vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+        return vetService.findAll();
     }
 
 }
