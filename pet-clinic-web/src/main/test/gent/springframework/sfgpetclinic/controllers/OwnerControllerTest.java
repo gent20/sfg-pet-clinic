0df94788b56da37ee6cf559a66ae0e78cb198492
gent20 <123qweasdbnM>
2020-07-28 14:08:41 +0200

    added a test on OwnerControllerTest . #65

--- pet-clinic-web/src/main/test/gent/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/main/test/gent/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -101,6 +101,19 @@ class OwnerControllerTest {
     }
 
     @Test
+    void processFindFormEmptyReturnMany() throws Exception {
+        when(ownerService.findAllByLastNameLike(anyString()))
+                .thenReturn(Arrays.asList(Owner.builder().id(1l).build(),
+                        Owner.builder().id(2l).build()));
+
+        mockMvc.perform(get("/owners")
+                .param("lastName",""))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownersList"))
+                .andExpect(model().attribute("selections", hasSize(2)));;
+    }
+
+    @Test
     void initUpdateOwnerForm() throws Exception {
         when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1l).build());
 
